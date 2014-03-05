7c527f034c4a275e5e56f225e18f48494a906f8b
Christopher Jimenez Valverde <cjimenez@rivetlogic.com>
2014-03-05 09:27:44 -0600

    LRA-91

--- themes/hr-portal-blue-responsive-theme/docroot/_diffs/js/main.js
+++ themes/hr-portal-blue-responsive-theme/docroot/_diffs/js/main.js
@@ -52,24 +52,5 @@ AUI().ready(
         if (navigation) {
             navigation.plug(Liferay.NavigationInteraction);
         }
-
-        
-        /* var navigation = A.one('#navigation');
-
-        if (navigation) {
-            navigation.plug(Liferay.NavigationInteraction);
-        }
-
-        var siteBreadcrumbs = A.one('#breadcrumbs');
-
-        if (siteBreadcrumbs) {
-            siteBreadcrumbs.plug(A.Hudcrumbs);
-        }
-
-        var signIn = A.one('li.sign-in a');
-
-        if (signIn && signIn.getData('redirect') !== 'true') {
-            signIn.plug(Liferay.SignInModal);
-        }*/ 
     }
 );
